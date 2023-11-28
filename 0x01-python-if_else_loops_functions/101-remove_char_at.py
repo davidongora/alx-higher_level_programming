af2434ed9e02ac83e52b79cc5dda326341bca7cd
DavidOngora <ongoradavid5.gmail.com>
2023-11-29 01:12:39 +0300

    removed trailling whitespace

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     modified_str = ""
-
     for i in range(len(str)):
-        
         if i == n:
             continue
-
         modified_str += str[i]
-    
     return modified_str
