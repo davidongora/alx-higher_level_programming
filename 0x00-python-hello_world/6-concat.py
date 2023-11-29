158163c8a1b3095eb95b1ffa6328142559d00127
DavidOngora <ongoradavid5.gmail.com>
2023-11-29 17:03:13 +0300

    made it print holberton 3 times

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 = str1 + " " + str2
-print("Welcome to {}!".format(str1*3))
+str0 = "Holberton School"
+str2 , str3= "School" , "Holberton"
+str1 = str0*3, "\n" [:9] + str3
+print(f"{str1}")
