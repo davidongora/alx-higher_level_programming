160490bff5cbdc9bc633fa5f9afe1073cdb9d159
DavidOngora <ongoradavid5.gmail.com>
2023-11-29 19:08:43 +0300

    zen of python

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-import this
+print("""Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+Except rise from the ashes of defeat.
+The Zen of Python, by Tim Peters.""")
