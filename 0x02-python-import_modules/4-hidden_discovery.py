fb05bf1e8725abea30e5c4b512425c3c1a0ce2f4
DavidOngora <ongoradavid5.gmail.com>
2023-12-01 02:06:42 +0300

    pycodestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,9 @@
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
-    import hidden_4
+    import hidden.pyc
 
-    names = dir(hidden_4)
+    names = dir(hidden_4.pyc)
     for name in names:
         if name[:2] != "__":
             print(name)
