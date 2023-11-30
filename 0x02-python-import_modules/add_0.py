48a8c8a97b5bf1ffbba1de2ee5914b51f4bdcfc2
DavidOngora <ongoradavid5.gmail.com>
2023-12-01 01:24:41 +0300

    functions to be imported by 0-add

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+def add (a, b):
+    """an addition function
+    
+    Args:
+        a: int holder
+        b: second int
+        
+    Returns:
+        The return value. a+b"""
+    
+    return a + b
