21fe1b48efac043a7fac8837c46ab1ad7ed050a9
DavidOngora <ongoradavid5.gmail.com>
2023-12-01 10:41:25 +0300

    removed depreceated module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
     import imp
     filename_pyc = 'hidden_4.pyc'
-    for i in dir(filename_pyc):
-        if i[0] != '__':
+    module = filename_pyc
+    for i in dir(module):
+        if not i.startswith("__"):
             print("{}".format(i))
