63cb02cc2f59c71afcd0d2a0e0957cfbec680d41
DavidOngora <ongoradavid5.gmail.com>
2023-12-01 10:04:13 +0300

    introduced new var and string formats

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,5 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
+    total += sum(int(sys.argv[i]) for i in range(1, len(sys.argv)))
     print("{}".format(total))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,9 @@
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
-    import hidden.pyc
+    import imp
 
-    names = dir(hidden_4.pyc)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    names = 'hidden_4.pyc'
+    for i in dir(filename_pyc):
+        if i[0] != '_':
+            print("{}".format(i))
