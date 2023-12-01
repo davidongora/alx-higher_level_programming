48d923a0200b15d680d43b42ed7fbdd0ac8b0af5
DavidOngora <ongoradavid5.gmail.com>
2023-12-01 10:18:56 +0300

    not to print files starting with __

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     import imp
     filename_pyc = 'hidden_4.pyc'
     for i in dir(filename_pyc):
-        if i[0] != '_':
+        if i[0] != '__':
             print("{}".format(i))
