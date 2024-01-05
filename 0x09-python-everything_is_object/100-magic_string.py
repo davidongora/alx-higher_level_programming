8d25d4d9d0a61df71c2a5145f973ef7b9967b4ca
DavidOngora <ongoradavid5.gmail.com>
2024-01-05 22:03:34 +0300

    python objects

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def magic_string():
+    for i in range(10):
+        return "BestSchool" * i
+        
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def copy_list(l):
+    list1 = [1,2,3]
+    return list1[:]
