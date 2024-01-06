5a283fac4602904b19ae8dd696b2cb0fd3f0ba02
DavidOngora <ongoradavid5.gmail.com>
2024-01-06 07:38:49 +0300

    changed the set attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class LockedClass:
-    __slots__ = ["first_name"]
-    
+    def __setattr__(self, name, value):
+        if not hasattr(self, 'first_name') and name != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+        object.__setattr__(self, name, value)
