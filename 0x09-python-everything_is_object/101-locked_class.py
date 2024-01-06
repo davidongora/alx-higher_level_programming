d5c75adfa13917a521f6877c4b4ba0e694fb9fb4
DavidOngora <ongoradavid5.gmail.com>
2024-01-06 07:46:07 +0300

    fix: removed extra doc strings

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""this is a locked class where the \
-    first attribute has to be first_name"""
 class LockedClass:
     """first attribute must be named first_class"""
     __slot__ = ["first_name"]
