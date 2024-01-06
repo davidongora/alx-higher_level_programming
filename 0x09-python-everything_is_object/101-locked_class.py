50f62ddd0f166b1bbfaf10bc00410556f71fd65f
DavidOngora <ongoradavid5.gmail.com>
2024-01-06 07:44:27 +0300

    fix:documented line break

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""this is a locked class where the first attribute has to be first_name"""
+"""this is a locked class where the \
+    first attribute has to be first_name"""
 class LockedClass:
     """first attribute must be named first_class"""
     __slot__ = ["first_name"]
