92407b6625428387505f17681f3e4acb2b734f19
DavidOngora <ongoradavid5.gmail.com>
2024-01-07 06:45:48 +0300

    extra line

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Returns an integer: the addition of a and b"""
+"""this module has a function that adds up 2 integrs"""
 def add_integer(a, b=98):
+    """Returns an integer: the addition of a and b"""
+
     if isinstance(a, (float, int)) and isinstance(b, (float, int)):
         return int(a) + int(b)
     else:
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module has a function that prints names"""
+def say_my_name(first_name, last_name=""):
+    """a function that prints My name is <first name> <last name>"""
+    if isinstance(first_name,last_name (str)):
+        print(f"my name is {first_name} {last_name}")
+    else:
+        raise TypeError("{first_name}, {last_name} must be a string or last_name must be a string")
