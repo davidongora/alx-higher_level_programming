f3db67e0c2e3787b10aff0fbb954df932e017475
DavidOngora <ongoradavid5.gmail.com>
2024-01-08 20:34:47 +0300

    added more spaces

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -19,6 +19,8 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If either of the arguments not an integer or a float
     """
+    
+    
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
